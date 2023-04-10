38ad000e68328f9240479e83970e9d303c914ef8
Hamza Jrifi <hjrifi@e1r7p4.1337.ma>
2023-04-10 11:40:17 +0000

    checked sever block and matching location

--- response/response.hpp
+++ response/response.hpp
@@ -6,26 +6,38 @@
 #include <sys/stat.h>
 
 struct client_info;
-
-struct status_code{
-    std::map<int, std::string> statusCodes;
-    
-    status_code();
-};
+struct config_file;
 
 class  responceClient{
     
     public:
     client_info *client;
     client_info *next;
-    std::string res_Send;
+
+    char    buffer_response[1024];
+    char    statusCode[20];
+    int     nbrstat;
+    char    res_body[1024];
+    int     contenet_lenght;
+    char    *content_type;
+    size_t  centenet_lenght;
+    ///
+    char    *mediaType;
+    /// block server 
+    std::string root;
+    std::string index;
+    std::vector<config_file> &block_server;
+
     std::map<std::string, int (*)(std::string)> methodeFunction;
-    responceClient();
-    int     checkUri(std::string);
-    static int getMethode(std::string methode);
-    static int postMethode(std::string methode);
-    static int deleteMethode(std::string methode);
-    int     ft_response(responceClient *res_client);
+    std::map<int, std::string> statusCodes;
+
+    responceClient(std::vector<config_file> &blockServer);
+    int         checkUri(std::string);
+    static int  getMethode(std::string methode);
+    static int  postMethode(std::string methode);
+    static int  deleteMethode(std::string methode);
+    int         ft_response(responceClient *res_client);
+    int         noLocation();
 };
 
 
