474e8654296113fbd71352758486399124a09e4f
Hamza Jrifi <hjrifi@e1r6p5.1337.ma>
2023-04-17 15:55:19 +0000

    method is done withot cgi

--- pages/index.php
+++ pages/index.php
@@ -1,4 +1,12 @@
 <?php
-echo "My first PHP script!";
+echo getcwd();
+$url = parse_url('www.domain.com?test=1&test2=2&test3=3');
+parse_str($url['query'], $keyvalue);
+
+//   $NAME = $_GET['name']; // Gets ?name=value
+// var_dump($keyvalue);
+// echo "\n";
+// echo ${NAME};
+// echo $keyvalue[test2];
 //  phpinfo();
 ?>
